36ee0540c1843d1d05d64f29302ac38787a6bd85
Kazuma Saito <kazu15870@gmail.com>
2023-09-03 17:13:27 -0700

    complete HW02-partA

--- src/HW02A.java
+++ src/HW02A.java
@@ -1,27 +1,97 @@
 /*
  * Title: Hw02A.java
- * Abstract: This program print a Horizontal and Vertical Histogram
+ * Abstract: This program calculates the student average score
  * Author: Kazuma Saito
  * Email: kasaito@csumb.edu
- * Estimate: 1.5h
- * Date: 8/29/2023
+ * Estimate: 2h
+ * Date: 9/03/2023
  */
+
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class HW02A {
-  public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter input file name: ");
         String fileName = scanner.nextLine();
         scanner.close();
 
+        // read input file
         FileReader fr = new FileReader(fileName);
         Scanner fs = new Scanner(fr);
-        String test = fs.nextLine();
 
-        System.out.println(test);
+        System.out.println("--------------------------------------------------");
+        System.out.println("  Course Report: Quiz Average");
+        System.out.println("--------------------------------------------------");
+
+        boolean stopCount = true;
+
+        while (stopCount) {
+            String name = fs.next();
+
+            if (!name.equals("STOP")) {
+                int studentNumber = fs.nextInt();
+
+                // get scores
+                int scoreNum = 5;
+                double scores[] = new double[scoreNum];
+
+                for (int j = 0; j < scoreNum; j++) {
+                    scores[j] = fs.nextDouble();
+                }
+
+                Student st = new Student(name, studentNumber);
+                st.getScores(scores);
+                st.setAverage();
+                System.out.println(st);
+            }
+            // STOP
+            else {
+                stopCount = false;
+            }
+        }
         fs.close();
-  }
+
+        System.out.println("--------------------------------------------------");
+    }
+}
+
+/**
+ * Student
+ */
+class Student {
+    private String name;
+    private int studentNumber;
+    private double[] scores;
+    private double average;
+
+    public Student(String n, int sn) {
+        name = n;
+        studentNumber = sn;
+    }
+
+    // store scores
+    public void getScores(double[] sc) {
+        scores = sc;
+    }
+
+    // score average
+    public void setAverage() {
+        double total = 0;
+        // To sort in ascending order
+        Arrays.sort(scores);
+
+        for (int i = scores.length - 1; i >= 1; i--) {
+            total += scores[i];
+        }
+
+        average = total / 4;
+    }
+
+    public String toString() {
+        return "  " + name + "(" + studentNumber + ")" + ": " + average;
+    }
 }
--- src/HW02B.java
+++ src/HW02B.java
@@ -1,25 +1,25 @@
-import java.util.Random;
-import java.util.Scanner;
-import java.util.concurrent.locks.Lock;
-
-public class HW02B {
-  public static void main(String[] args) {
-    Random r = new Random();
-    Scanner in = new Scanner(System.in);
-    Lock l = new Lock(r.nextInt(40) + 1,
-    r.nextInt(40) + 1,
-    r.nextInt(40) + 1);
-
-    while(l.isLocked()) {
-      int guess;
-      do {
-      System.out.print("Enter guess: ");
-      guess = in.nextInt();
-      } while(guess < 1 || guess > 40);
-      l.guess(guess);
-    }
-
-    System.out.println(l);
-    System.out.println(l.guesses());
-  }
-}
+//import java.util.Random;
+//import java.util.Scanner;
+//import java.util.concurrent.locks.Lock;
+//
+//public class HW02B {
+//  public static void main(String[] args) {
+//    Random r = new Random();
+//    Scanner in = new Scanner(System.in);
+//    Lock l = new Lock(r.nextInt(40) + 1,
+//    r.nextInt(40) + 1,
+//    r.nextInt(40) + 1);
+//
+//    while(l.isLocked()) {
+//      int guess;
+//      do {
+//      System.out.print("Enter guess: ");
+//      guess = in.nextInt();
+//      } while(guess < 1 || guess > 40);
+//      l.guess(guess);
+//    }
+//
+//    System.out.println(l);
+//    System.out.println(l.guesses());
+//  }
+//}
