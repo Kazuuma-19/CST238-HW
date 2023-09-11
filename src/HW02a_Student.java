8a8d02fe44a16466e8d51ee82bfd8c9bfe3b1ab8
Kazuma Saito <kazu15870@gmail.com>
2023-09-10 21:12:03 -0700

    complete HW03a

--- src/HW02a_Student.java
+++ src/HW02a_Student.java
@@ -1,97 +1,96 @@
-/*
- * Title: HW02a_Student.java
- * Abstract: This program calculates the student average score
- * Author: Kazuma Saito
- * Email: kasaito@csumb.edu
- * Estimate: 2h
- * Date: 9/03/2023
- */
-
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Scanner;
-
-public class HW02a_Student {
-    public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter input file name: ");
-        String fileName = scanner.nextLine();
-        scanner.close();
-
-        // read input file
-        FileReader fr = new FileReader(fileName);
-        Scanner fs = new Scanner(fr);
-
-        System.out.println("--------------------------------------------------");
-        System.out.println("  Course Report: Quiz Average");
-        System.out.println("--------------------------------------------------");
-
-        boolean stopCount = true;
-
-        while (stopCount) {
-            String name = fs.next();
-
-            if (!name.equals("STOP")) {
-                int studentNumber = fs.nextInt();
-
-                // get scores
-                int scoreNum = 5;
-                double scores[] = new double[scoreNum];
-
-                for (int j = 0; j < scoreNum; j++) {
-                    scores[j] = fs.nextDouble();
-                }
-
-                Student st = new Student(name, studentNumber);
-                st.getScores(scores);
-                st.setAverage();
-                System.out.println(st);
-            }
-            // STOP
-            else {
-                stopCount = false;
-            }
-        }
-        fs.close();
-
-        System.out.println("--------------------------------------------------");
-    }
-}
-
-/**
- * Student
- */
-class Student {
-    private String name;
-    private int studentNumber;
-    private double[] scores;
-    private double average;
-
-    public Student(String n, int sn) {
-        name = n;
-        studentNumber = sn;
-    }
-
-    // store scores
-    public void getScores(double[] sc) {
-        scores = sc;
-    }
-
-    // score average
-    public void setAverage() {
-        double total = 0;
-        // To sort in ascending order
-        Arrays.sort(scores);
-
-        for (int i = scores.length - 1; i >= 1; i--) {
-            total += scores[i];
-        }
-
-        average = total / 4;
-    }
-
-    public String toString() {
-        return "  " + name + "(" + studentNumber + ")" + ": " + average;
-    }
-}
+///*
+// * Title: HW02a_Student.java
+// * Abstract: This program calculates the student average score
+// * Author: Kazuma Saito
+// * Email: kasaito@csumb.edu
+// * Estimate: 2h
+// * Date: 9/03/2023
+// */
+//
+//import java.io.FileReader;
+//import java.io.IOException;
+//import java.util.Scanner;
+//
+//public class HW02a_Student {
+//    public static void main(String[] args) throws IOException {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter input file name: ");
+//        String fileName = scanner.nextLine();
+//        scanner.close();
+//
+//        // read input file
+//        FileReader fr = new FileReader(fileName);
+//        Scanner fs = new Scanner(fr);
+//
+//        System.out.println("--------------------------------------------------");
+//        System.out.println("  Course Report: Quiz Average");
+//        System.out.println("--------------------------------------------------");
+//
+//        boolean stopCount = true;
+//
+//        while (stopCount) {
+//            String name = fs.next();
+//
+//            if (!name.equals("STOP")) {
+//                int studentNumber = fs.nextInt();
+//
+//                // get scores
+//                int scoreNum = 5;
+//                double scores[] = new double[scoreNum];
+//
+//                for (int j = 0; j < scoreNum; j++) {
+//                    scores[j] = fs.nextDouble();
+//                }
+//
+//                Student st = new Student(name, studentNumber);
+//                st.getScores(scores);
+//                st.setAverage();
+//                System.out.println(st);
+//            }
+//            // STOP
+//            else {
+//                stopCount = false;
+//            }
+//        }
+//        fs.close();
+//
+//        System.out.println("--------------------------------------------------");
+//    }
+//}
+//
+///**
+// * Student
+// */
+////class Student {
+////    private String name;
+////    private int studentNumber;
+////    private double[] scores;
+////    private double average;
+////
+////    public Student(String n, int sn) {
+////        name = n;
+////        studentNumber = sn;
+////    }
+////
+////    // store scores
+////    public void getScores(double[] sc) {
+////        scores = sc;
+////    }
+////
+////    // score average
+////    public void setAverage() {
+////        double total = 0;
+////        // To sort in ascending order
+////        Arrays.sort(scores);
+////
+////        for (int i = scores.length - 1; i >= 1; i--) {
+////            total += scores[i];
+////        }
+////
+////        average = total / 4;
+////    }
+////
+////    public String toString() {
+////        return "  " + name + "(" + studentNumber + ")" + ": " + average;
+////    }
+////}
---
+++ src/HW03a_Student.java
@@ -0,0 +1,154 @@
+/*
+ * Title: HW03a_Student.java
+ * Abstract: This program is a number guessing game
+ * Author: Kazuma Saito
+ * Email: kasaito@csumb.edu
+ * Estimate: 2h
+ * Date: 09/10/2023
+ */
+public class HW03a_Student {
+    public static void main(String[] args) {
+        Course c1 = new Course("Intro to Everything", 123, "Dr. Gross");
+        Student s1 = new Student("Jane", 1234);
+        Student s2 = new Student("Phillipe", 1235);
+        Student s3 = new Student("Lu", 1236);
+        Student s4 = new Student("Enrique", 1237);
+        s1.setName("Kazuma");
+        c1.add(s1);
+        c1.add(s2);
+        c1.add(s3);
+        c1.add(s4);
+        c1.print();
+        c1.remove(s1);
+        c1.print();
+//        System.out.println(s1);
+//        System.out.println(c1);
+    }
+}
+
+class Student {
+    private String name;
+    private int id;
+
+    public Student(String n, int i) {
+        name = n;
+        id = i;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String n) {
+        name = n;
+    }
+
+    // compare id
+    public boolean equals(Student st) {
+        return id == st.getId();
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Name: " + name);
+        sb.append(" - ");
+        sb.append("ID: " + id);
+        System.out.println();
+        return sb.toString();
+    }
+}
+
+class Course {
+    private String title;
+    private int number;
+    private String instructorName;
+    private int enrolledNumber;
+    private int classCapacity;
+    private Student[] roster;
+
+    public Course(String t, int n, String i) {
+        title = t;
+        number = n;
+        instructorName = i;
+        enrolledNumber = 0;
+        classCapacity = 30;
+        roster = new Student[classCapacity];
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getInstructorName() {
+        return instructorName;
+    }
+
+    public boolean add(Student s) {
+        boolean isEnrolled = true;
+
+        for (int i = 0; i < enrolledNumber; i++) {
+            if (roster[i].equals(s) || enrolledNumber == classCapacity) {
+                isEnrolled = false;
+            }
+        }
+
+        if (isEnrolled) {
+            roster[enrolledNumber++] = s;
+        }
+
+        return isEnrolled;
+    }
+
+    public boolean remove(Student s) {
+        boolean isEnrolled = false;
+
+        for (int i = 0; i < enrolledNumber; i++) {
+            if (roster[i].equals(s)) {
+                for (int j = i; j < enrolledNumber - 1; j++) {
+                    roster[j] = roster[j + 1];
+                }
+
+                isEnrolled = true;
+                enrolledNumber--;
+            }
+        }
+        return isEnrolled;
+    }
+
+    public void print() {
+        System.out.println("Course Title: " + title);
+        System.out.println("Course Number: " + number);
+        System.out.println("Instructor: " + instructorName);
+        System.out.println("Enrollment: " + enrolledNumber);
+
+        for (int i = 0; i < enrolledNumber; i++) {
+            System.out.print("  Name: " + roster[i].getName());
+            System.out.print(" - ");
+            System.out.print("ID: " + roster[i].getId());
+            System.out.println();
+        }
+    }
+
+    // compares the title and number
+    public boolean equals(Course co) {
+        return number == co.number && title.equals(co.title);
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Course Title: " + title);
+        sb.append("\n");
+        sb.append("Course Number: " + number);
+        sb.append("\n");
+        sb.append("Instructor: " + instructorName);
+        return sb.toString();
+    }
+}
