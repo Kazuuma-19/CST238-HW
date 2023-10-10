06eb8dbc0e04fc21c497b065b6f3afea52318039
Kazuma Saito <kazu15870@gmail.com>
2023-10-10 14:10:19 -0700

    HW05-parB

--- src/HW05/partB/Main.java
+++ src/HW05/partB/Main.java
@@ -1,4 +1,35 @@
 package HW05.partB;
 
 public class Main {
+    public static void main(String[] args) {
+        Queue q = new Queue();
+        System.out.println("Capacity (should be 5): " + q.getCapacity());
+        System.out.println("Size (should be 0): " + q.getSize());
+        System.out.println();
+        for (int i = 0; i < q.getCapacity(); i++) {
+            q.enqueue(q.getCapacity() - i);
+        }
+        System.out.println("Size (should be 5): " + q.getSize());
+        System.out.println(q.printable());
+        System.out.println();
+        for (int i = 0; i < 3; i++) {
+            q.dequeue();
+        }
+        System.out.println("Size (should be 2): " + q.getSize());
+        System.out.println(q.printable());
+        System.out.println();
+//        q.enqueue(-1);
+//        q.enqueue(-2);
+//        q.enqueue(-3);
+//        System.out.println(q.printable());
+//        q.enqueue(-4);
+////        q.enqueue(-5);
+
+        for (int i = 0; i < 5; i++) {
+            q.enqueue((i + 1) * -1);
+        }
+        System.out.println("Capacity (should be 11): " + q.getCapacity());
+        System.out.println("Size (should be 7): " + q.getSize());
+        System.out.println(q.printable());
+    }
 }
--- src/HW05/partB/Queue.java
+++ src/HW05/partB/Queue.java
@@ -1,15 +1,15 @@
 /*
  * Title: Queue.java
- * Abstract: This program convert decimal to binary.
+ * Abstract: This program resize queue.
  * Author: Kazuma Saito
  * Email: kasaito@csumb.edu
- * Estimate: 30m
- * Date: 09/23/2023
+ * Estimate: 1.5h
+ * Date: 10/11/2023
  */
 package HW05.partB;
 
 public class Queue {
-    public static final int QUEUE_CAPACITY = 10;
+    public static final int QUEUE_CAPACITY = 6;
     private int[] data;
     private int front;
     private int back;
@@ -27,8 +27,10 @@ public class Queue {
     public void enqueue(int value) {
         int newBack = (back + 1) % data.length;
         if (newBack == front) {
-            System.out.println("**Queue full - cannot enqueue " + value + "**");
-            return;
+//            System.out.println("**Queue full - cannot enqueue " + value + "**");
+//            return;
+            resize();
+            newBack = (back + 1) % data.length;
         }
         data[back] = value;
         back = newBack;
@@ -39,7 +41,7 @@ public class Queue {
             System.out.println("**Queue empty - cannot dequeue**");
             return;
         }
-        // data[front] = null; // only if stack contains objects
+        data[front] = 0;
         front = (front + 1) % data.length;
     }
 
@@ -49,6 +51,52 @@ public class Queue {
             return 0;
         }
         return data[front];
+    }
+
+    public int getCapacity() {
+        return data.length - 1;
+    }
+
+    public int getSize() {
+        if (front > back) {
+            return (data.length - front) + (back + 1);
+        } else {
+            return back - front;
+        }
+    }
+
+    public void resize() {
+        int[] temp = new int[data.length + 1];
+        for (int i = 0; i < data.length; i++) {
+            if (i < back) {
+                temp[i] = data[i];
+            } else {
+                temp[i + 1] = data[i];
+            }
+        }
+//        for (int d :
+//                temp) {
+//            System.out.print(d + ", ");
+//        }
+//        System.out.println();
+//        System.out.println();
+
+        data = temp;
+        front++;
+
+        System.out.println("Array is resized");
+        System.out.println();
+    }
+
+    public String printable() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("(f: " + front + ", ");
+        sb.append("b: " + back + ") ");
+        sb.append("Contents: ");
+        for (int i = 0; i < data.length; i++) {
+            sb.append(data[i]).append(" ");
+        }
+        return sb.toString();
     }
 
     public String toString() {
