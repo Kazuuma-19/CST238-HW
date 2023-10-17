6a2c0347c18c5ff6d97eba6beeec5a22d0a5e245
Kazuma Saito <kazu15870@gmail.com>
2023-10-17 10:26:42 -0700

    complete: HW06-partB

--- src/HW06/partBC/Main.java
+++ src/HW06/partBC/Main.java
@@ -2,19 +2,25 @@ package HW06.partBC;
 
 public class Main {
     public static void main(String[] args) {
-//        Time t1 = new Time(12, 55);
-//        Time t2 = new Time(29, 34);
-//        Time t3 = new Time(3, 45);
-//        Time t4 = new Time(22, 33);
-//        System.out.println(t1.toStandard() + " - " + t1);
-//        System.out.println(t2.toStandard() + " - " + t2);
-//        System.out.println(t3.toStandard() + " - " + t3);
-//        System.out.println(t4.toStandard() + " - " + t4);
-//        Time t5 = t3.add(t4);
-//        System.out.println(t5.toStandard() + " - " + t5);
-//        Time t6 = new Time();
-//        Time t7 = new Time(12);
-//        System.out.println(t6.toStandard() + " - " + t6);
-//        System.out.println(t7.toStandard() + " - " + t7);
+        Time t1 = new Time(12, 55);
+        Time t2 = new Time(29, 34);
+        Time t3 = new Time(3, 45);
+        Time t4 = new Time(22, 33);
+        System.out.println(t1.toStandard() + " - " + t1);
+        System.out.println(t2.toStandard() + " - " + t2);
+        System.out.println(t3.toStandard() + " - " + t3);
+        System.out.println(t4.toStandard() + " - " + t4);
+        Time t5 = t3.add(t4);
+        System.out.println(t5.toStandard() + " - " + t5);
+        Time t6 = new Time();
+        Time t7 = new Time(12);
+        System.out.println(t6.toStandard() + " - " + t6);
+        System.out.println(t7.toStandard() + " - " + t7);
+
+        System.out.println();
+        Time t8 = new Time(23, 45);
+        Time t9 = new Time(0, 16);
+        Time t10 = t8.add(t9);
+        System.out.println(t10.toStandard() + " - " + t10);
     }
 }
--- src/HW06/partBC/Time.java
+++ src/HW06/partBC/Time.java
@@ -10,4 +10,70 @@
 package HW06.partBC;
 
 public class Time {
+    private int hours;
+    private int minutes;
+
+    public Time() {
+        this(0, 0);
+    }
+
+    public Time(int hours) {
+        this(hours, 0);
+    }
+
+    public Time(int hours, int minutes) {
+        if (isValid(hours, minutes)) {
+            this.hours = hours;
+            this.minutes = minutes;
+        } else {
+            this.hours = 0;
+            this.minutes = 0;
+        }
+    }
+
+    public boolean isValid(int hours, int minutes) {
+        return hours >= 0 && hours <= 23 && minutes >= 0 && minutes <= 59;
+    }
+
+    public String toStandard() {
+        StringBuilder sb = new StringBuilder();
+        // hours
+        if (this.hours == 0 || this.hours == 12) {
+            sb.append("12");
+        } else {
+            sb.append(this.hours % 12);
+        }
+
+        sb.append(":");
+
+        // minutes
+        sb.append(String.format("%02d", this.minutes));
+        // AM, PM
+        sb.append(this.hours < 12 ? "AM" : "PM");
+
+        return sb.toString();
+    }
+
+    public String toMilitary() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(String.format("%02d", this.hours));
+        sb.append(String.format("%02d", this.minutes));
+
+        return sb.toString();
+    }
+
+    public Time add(Time t2) {
+        int totalMinutes = this.minutes + t2.minutes;
+        int totalHours = this.hours + t2.hours + (totalMinutes / 60);
+
+        totalMinutes %= 60;
+        totalHours %= 24;
+
+        return new Time(totalHours, totalMinutes);
+    }
+
+    public String toString() {
+        return toMilitary();
+    }
 }
